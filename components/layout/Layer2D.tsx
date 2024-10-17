0d631acfddbeb6759d32f4a647a7cb7b7d5690a7
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2024-10-17 15:15:02 +0900

    feat : fade in & out on scroll event

--- components/layout/Layer2D.tsx
+++ components/layout/Layer2D.tsx
@@ -1,38 +1,55 @@
 "use client";
-import { motion, useScroll, useSpring } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
-const Layer2D = () => {
-  const { scrollYProgress } = useScroll();
-  const pathLength = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
+const Item = ({ children }: { children: React.ReactNode }) => {
+  const ref = useRef(null);
+  const [viewportHeight, setViewportHeight] = useState(0);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
   });
 
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportHeight(window.innerHeight);
+    };
+
+    handleResize(); // Set initial height
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const y = useTransform(scrollYProgress, [0, 0.2, 0.8, 1], ["0px", "0px", `${viewportHeight * 0.5}px`, "0px"]);
+  const opacity = useTransform(scrollYProgress, [0, 0.25, 0.6, 0.8], [0, 1, 1, 0]);
+  const scale = useTransform(scrollYProgress, [0, 0.3, 0.6, 1], [0.6, 1, 1, 0.6]);
+
   return (
-    <div style={{ position: "relative" }}>
-      <div className="h-[70vh]">1</div>
-      <div className="">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="100"
-          height="100%"
-          viewBox="0 0 100 1000"
-          // preserveAspectRatio="xMidYMid meet"
-          style={{ position: "sticky", top: "0%", left: "20%", height: "100vh" }}
-          className="border border-white/20"
-        >
-          <motion.path
-            d="M100 0 L 100 100 C 100 250, 0 250, 0 400 V800"
-            fill="transparent"
-            stroke="white"
-            strokeWidth="2"
-            style={{ pathLength: pathLength }}
-          />
-        </svg>
-        <div style={{ height: "100vh", position: "relative" }}></div>
-      </div>
-      <div className="h-[800px]">2</div>
+    <motion.div ref={ref} className="h-[100vh] flex items-start justify-center w-full">
+      <motion.div
+        style={{ y, opacity, scale }}
+        className="border w-full xl:w-[400px] h-[320px] flex items-center justify-center"
+      >
+        {children}
+      </motion.div>
+    </motion.div>
+  );
+};
+
+const Layer2D = () => {
+  return (
+    <div>
+      <div className="h-[100vh]">Self description</div>
+
+      <Item key={1}>First Project</Item>
+      <Item key={2}>Second Project</Item>
+      <Item key={3}>Third Project</Item>
+
+      <div className="h-[70vh]">Contact</div>
     </div>
   );
 };
